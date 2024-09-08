83c4d722ad1dcc36149f9d8df85ed9861aef6611
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-09-08 21:48:14 +0100

    Added requirements.txt file

--- src/auth-service/server.py
+++ src/auth-service/server.py
@@ -1,13 +1,17 @@
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 server = Flask(__name__)
 mysql = MySQL(server)
 
-# config
-server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
+# MySQL configuration from environment variables
+server.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+server.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
