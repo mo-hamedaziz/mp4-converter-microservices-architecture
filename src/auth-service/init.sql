f0d708f6920a2316e325a38ebd67df2f54bfe3f8
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-09-08 22:07:39 +0100

    Added docker-compose file

--- src/auth-service/init.sql
+++ src/auth-service/init.sql
@@ -1,15 +1,16 @@
 CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 
-CREATE DATABASE authdb;
+CREATE DATABASE IF NOT EXISTS authdb;
 
-GRANT ALL PRIVILEGES ON authdb.* TO 'auth_user'@'localhost';
+GRANT ALL PRIVILEGES ON authdb.* TO 'auth_user'@'%';
 
 USE authdb;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (email, password) VALUES ('aziz@email.com', 'Admin123');
+INSERT INTO user (email, password) VALUES ('aziz@email.com', 'Admin123')
+ON DUPLICATE KEY UPDATE password = VALUES(password);
